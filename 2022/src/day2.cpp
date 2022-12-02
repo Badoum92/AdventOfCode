8a145b513fa7ef6fcfa1806146ed2fdf66312cd3
Loic Bellonnet-Mottet <loic.bellonnet-mottet@epita.fr>
2022-12-02 12:59:14 +0100

    day2

--- 2022/src/day2.cpp
+++ 2022/src/day2.cpp
@@ -2,11 +2,31 @@
 
 #include <string>
 
-const char* provided_paths[] = {"input/day0_provided"};
-uint64_t provided_expected1[] = {123};
-uint64_t provided_expected2[] = {124};
+const char* provided_paths[] = {"input/day2_provided"};
+uint64_t provided_expected1[] = {15};
+uint64_t provided_expected2[] = {12};
 
-const char* real_input = "input/day0";
+const char* real_input = "input/day2";
+
+#define ROCK_A     'A'
+#define PAPER_A    'B'
+#define SCISSORS_A 'C'
+
+#define ROCK_B     'X'
+#define PAPER_B    'Y'
+#define SCISSORS_B 'Z'
+
+#define LOSE_B 'X'
+#define DRAW_B 'Y'
+#define WIN_B  'Z'
+
+#define ROCK     1
+#define PAPER    2
+#define SCISSORS 3
+
+#define LOSE 0
+#define DRAW 3
+#define WIN  6
 
 using Input = std::vector<std::string>;
 
@@ -15,14 +35,88 @@ Input parse_input(const char* path)
     return input_to_lines(path);
 }
 
+uint64_t score1(char a, char b)
+{
+    switch (a)
+    {
+    case ROCK_A: {
+        if (b == ROCK_B)
+            return ROCK + DRAW;
+        else if (b == PAPER_B)
+            return PAPER + WIN;
+        else if (b == SCISSORS_B)
+            return SCISSORS + LOSE;
+    }
+    case PAPER_A: {
+        if (b == ROCK_B)
+            return ROCK + LOSE;
+        else if (b == PAPER_B)
+            return PAPER + DRAW;
+        else if (b == SCISSORS_B)
+            return SCISSORS + WIN;
+    }
+    case SCISSORS_A: {
+        if (b == ROCK_B)
+            return ROCK + WIN;
+        else if (b == PAPER_B)
+            return PAPER + LOSE;
+        else if (b == SCISSORS_B)
+            return SCISSORS + DRAW;
+    }
+    }
+    return 0;
+}
+
+uint64_t score2(char a, char b)
+{
+    switch (b)
+    {
+    case LOSE_B: {
+        if (a == ROCK_A)
+            return LOSE + SCISSORS;
+        else if (a == PAPER_A)
+            return LOSE + ROCK;
+        else if (a == SCISSORS_A)
+            return LOSE + PAPER;
+    }
+    case DRAW_B: {
+        if (a == ROCK_A)
+            return DRAW + ROCK;
+        else if (a == PAPER_A)
+            return DRAW + PAPER;
+        else if (a == SCISSORS_A)
+            return DRAW + SCISSORS;
+    }
+    case WIN_B: {
+        if (a == ROCK_A)
+            return WIN + PAPER;
+        else if (a == PAPER_A)
+            return WIN + SCISSORS;
+        else if (a == SCISSORS_A)
+            return WIN + ROCK;
+    }
+    }
+    return 0;
+}
+
 uint64_t step1(const Input& input)
 {
-    return std::stoull(input[0]);
+    uint64_t total = 0;
+    for (const auto& s : input)
+    {
+        total += score1(s[0], s[2]);
+    }
+    return total;
 }
 
 uint64_t step2(const Input& input)
 {
-    return std::stoull(input[0]) + 1;
+    uint64_t total = 0;
+    for (const auto& s : input)
+    {
+        total += score2(s[0], s[2]);
+    }
+    return total;
 }
 
 int main()
