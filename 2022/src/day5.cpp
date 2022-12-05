3d9cf8f22836d092de1dc08a89f9f0f07c5eff45
Loic Bellonnet-Mottet <loic.bellonnet-mottet@epita.fr>
2022-12-05 14:05:50 +0100

    day5

--- 2022/src/day5.cpp
+++ 2022/src/day5.cpp
@@ -2,27 +2,110 @@
 
 #include <string>
 
-const char* provided_paths[] = {"input/day0_provided"};
-uint64_t provided_expected1[] = {0};
-uint64_t provided_expected2[] = {0};
+const char* provided_paths[] = {"input/day5_provided"};
+const char* provided_expected1[] = {"CMZ"};
+const char* provided_expected2[] = {"MCD"};
 
-const char* real_input = "input/day0";
+const char* real_input = "input/day5";
 
-using Input = std::vector<std::string>;
+struct MoveOperation
+{
+    uint32_t amount;
+    uint32_t from;
+    uint32_t to;
+};
+
+struct Input
+{
+    std::vector<std::vector<char>> stacks;
+    std::vector<MoveOperation> moves;
+};
 
 Input parse_input(const char* path)
 {
-    return input_to_lines(path);
+    Input input;
+    input.stacks.resize(10);
+    auto lines = input_to_lines(path);
+
+    size_t i = 0;
+    for (; i < lines.size(); ++i)
+    {
+        const auto& line = lines[i];
+        if (line[1] == '1')
+        {
+            i += 2;
+            break;
+        }
+
+        for (size_t j = 0; j < 10; ++j)
+        {
+            if (2 + j * 4 < line.size() && line[1 + j * 4] != ' ')
+            {
+                input.stacks[j + 1].push_back(line[1 + j * 4]);
+            }
+        }
+    }
+
+    for (auto& stack : input.stacks)
+    {
+        std::reverse(stack.begin(), stack.end());
+    }
+
+    for (; i < lines.size(); ++i)
+    {
+        const auto& line = lines[i];
+        MoveOperation& move = input.moves.emplace_back();
+        sscanf(line.c_str(), "move %u from %u to %u", &move.amount, &move.from, &move.to);
+    }
+
+    return input;
 }
 
-uint64_t step1(const Input& input)
+std::string step1(Input& input)
 {
-    return 0;
+    for (const auto& move : input.moves)
+    {
+        for (uint32_t i = 0; i < move.amount; ++i)
+        {
+            input.stacks[move.to].push_back(input.stacks[move.from].back());
+            input.stacks[move.from].pop_back();
+        }
+    }
+
+    std::string res;
+    for (const auto& stack : input.stacks)
+    {
+        if (!stack.empty())
+        {
+            res += stack.back();
+        }
+    }
+    return res;
 }
 
-uint64_t step2(const Input& input)
+std::string step2(Input& input)
 {
-    return 0;
+    for (const auto& move : input.moves)
+    {
+        for (uint32_t i = 0; i < move.amount; ++i)
+        {
+            input.stacks[move.to].push_back(input.stacks[move.from][input.stacks[move.from].size() - (move.amount - i)]);
+        }
+        for (uint32_t i = 0; i < move.amount; ++i)
+        {
+            input.stacks[move.from].pop_back();
+        }
+    }
+
+    std::string res;
+    for (const auto& stack : input.stacks)
+    {
+        if (!stack.empty())
+        {
+            res += stack.back();
+        }
+    }
+    return res;
 }
 
 int main()
@@ -31,12 +114,13 @@ int main()
     {
         Input input = parse_input(provided_paths[i]);
         auto [res1, time1] = time_function(step1, input);
+        input = parse_input(provided_paths[i]);
         auto [res2, time2] = time_function(step2, input);
 
         printf("Provided input #%zu:\n", i);
-        printf("    Step1: Expected: %llu  |  Result: %llu  |  Time: %llu ms  |  %s\n", provided_expected1[i], res1,
+        printf("    Step1: Expected: %s  |  Result: %s  |  Time: %llu ms  |  %s\n", provided_expected1[i], res1.c_str(),
                time1, res1 == provided_expected1[i] ? "OK" : "KO");
-        printf("    Step2: Expected: %llu  |  Result: %llu  |  Time: %llu ms  |  %s\n", provided_expected2[i], res2,
+        printf("    Step2: Expected: %s  |  Result: %s  |  Time: %llu ms  |  %s\n", provided_expected2[i], res2.c_str(),
                time2, res2 == provided_expected2[i] ? "OK" : "KO");
 
         if (res1 != provided_expected1[i] || res2 != provided_expected2[i])
@@ -47,8 +131,9 @@ int main()
 
     Input input = parse_input(real_input);
     auto [res1, time1] = time_function(step1, input);
+    input = parse_input(real_input);
     auto [res2, time2] = time_function(step2, input);
     printf("\n=================================\n");
-    printf("Step1: Result: %llu  |  Time: %llu ms\n", res1, time1);
-    printf("Step2: Result: %llu  |  Time: %llu ms\n", res2, time2);
+    printf("Step1: Result: %s  |  Time: %llu ms\n", res1.c_str(), time1);
+    printf("Step2: Result: %s  |  Time: %llu ms\n", res2.c_str(), time2);
 }
